ce3881f4c724fbe30bc9200aa73b6609fb7a72a7
unknown <m.buzzard87@gmail.com>
2021-08-13 11:58:28 -0400

    number of duplicates

---
+++ src/main/java/NumberOfDuplicates.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class NumberOfDuplicates {
+
+
+    public int numOfDuplicates(int[] dupArr) {
+
+        Set<Integer> dupSet = new HashSet<>();
+
+        for(int dup : dupArr) {
+            dupSet.add(dup);
+        }
+
+        return dupArr.length - dupSet.size();
+    }
+}
---
+++ src/test/java/NumberOfDuplicatesTest.java
@@ -0,0 +1,41 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NumberOfDuplicatesTest {
+
+    NumberOfDuplicates numDup = new NumberOfDuplicates();
+
+    @Test
+    public void testNumDup1() {
+
+        int[] dupArr = {1,2,3,4,5,3,2,1,4,5,7,4};
+
+        int actual = numDup.numOfDuplicates(dupArr);
+
+        assertEquals(6,actual);
+
+    }
+
+    @Test
+    public void testNumDup2() {
+
+        int[] dupArr = {1,2,3,4,5,6,7,8,9,10};
+
+        int actual = numDup.numOfDuplicates(dupArr);
+
+        assertEquals(0,actual);
+
+    }
+
+    @Test
+    public void testNumDup3() {
+
+        int[] dupArr = {1,1,1,1,1,1};
+
+        int actual = numDup.numOfDuplicates(dupArr);
+
+        assertEquals(5,actual);
+
+    }
+}
