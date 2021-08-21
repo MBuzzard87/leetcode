0a4007410983cc902fab1adceba570fc2e30f979
unknown <m.buzzard87@gmail.com>
2021-08-20 23:20:30 -0400

    add wrapper class/algo

---
+++ src/main/java/com/michaelbuzzard/leetcode/Wrapper.java
@@ -0,0 +1,47 @@
+package com.michaelbuzzard.leetcode;
+
+import java.io.*;
+
+public class Wrapper {
+
+    //Print a statement only allowing a max of 70 columns/characters per row
+    //If a word will be cut off then print that word on the next line instead of splitting the string
+
+
+
+    public static void main(String[] args) throws Exception {
+        File file = new File("src/ipsum.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        StringBuilder sb = new StringBuilder();
+        String str;
+
+        while((str = br.readLine()) != null) {
+            sb.append(str);
+        }
+
+        wrap(sb.toString(), 70);
+    }
+
+
+    public static void wrap(String sentence, int num) {
+
+        String[] splitString = sentence.split(" ");
+        StringBuilder curStr = new StringBuilder();
+
+        for (int i = 0; i < splitString.length; i++){
+            String tempStr = splitString[i];
+
+            if(tempStr.length() + curStr.length() + 1 <= num) {
+                tempStr = tempStr + " ";
+                curStr.append(tempStr);
+                System.out.print(tempStr);
+
+            } else {
+                System.out.print("\n" + tempStr + " ");
+                curStr = new StringBuilder(tempStr + " ");
+            }
+        }
+    }
+}
+
+//string w/ linebreaks
