ed02e07c109ef89795c4564cc81c2ba01d360330
unknown <m.buzzard87@gmail.com>
2021-09-30 23:37:42 -0400

    even digits test

--- src/test/java/com/michaelbuzzard/leetcode/NumWithEvenNumOfDigitsTest.java
+++ src/test/java/com/michaelbuzzard/leetcode/NumWithEvenNumOfDigitsTest.java
@@ -6,5 +6,29 @@ import static org.junit.Assert.*;
 
 public class NumWithEvenNumOfDigitsTest {
 
+    NumWithEvenNumOfDigits num = new NumWithEvenNumOfDigits();
 
+    @Test
+    public void testFindNumbers1() {
+        int[] arr1 = {12,345,2,6,7896};
+        int actual = num.findNumbers(arr1);
+
+        assertEquals(2,actual);
+    }
+
+    @Test
+    public void testFindNumbers2() {
+        int[] arr1 = {1,345,2,6,896};
+        int actual = num.findNumbers(arr1);
+
+        assertEquals(0,actual);
+    }
+
+    @Test
+    public void testFindNumbers3() {
+        int[] arr1 = {12,3456,32,26,7896};
+        int actual = num.findNumbers(arr1);
+
+        assertEquals(5,actual);
+    }
 }
